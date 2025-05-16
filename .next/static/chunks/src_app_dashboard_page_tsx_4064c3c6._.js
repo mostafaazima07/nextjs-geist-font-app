959898cfdf2fe79ae154c3285d8835a35c33de18
mostafaazima07 <info@thewebvalue.com>
2025-05-16 18:53:40 +0300

    Add .next/static/chunks/src_app_dashboard_page_tsx_4064c3c6._.js 1747410821910

---
+++ .next/static/chunks/src_app_dashboard_page_tsx_4064c3c6._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_ae4f28ce._.js",
+  "static/chunks/node_modules_75d6bef9._.js"
+],
+    source: "dynamic"
+});
